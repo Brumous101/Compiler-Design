8c53ffcaf810ca3358932ec32a8f99f73ea91dba
Kyle Johnson <Brumous101@gmail.com>
2021-04-08 18:34:04 -0400

    Add Completed lab 6

---
+++ Labs/Lab6/Compiler/bin/Debug/netcoreapp3.1/out.asm
@@ -0,0 +1,21 @@
+    default rel
+    section .text
+    global main
+main:
+    ; begin while loop at line yo 1
+lbl2:
+    push qword 1
+    pop rax
+    cmp rax, 0
+    je lbl1
+    push qword 0
+    pop rax
+    cmp rax,0
+    je lbl0
+    push qword 1
+    pop rax
+    ret
+lbl0:
+    jmp lbl2
+lbl1:
+    section .data
